e113abd0de5748c47dba832babcbc8cc353e95a6
Alex Komarov <komarov.aleksey@gmail.com>
2017-09-29 17:10:33 +0300

    Отображаются экраны игры Экраны меняются по нажатию alt+leftArrow/alt+RightArrow

---
+++ js/main.js
@@ -0,0 +1,74 @@
+const FIRST_PAGE_INDEX = 0;
+
+const templateNode = document.getElementById(`templates`);
+if (`content` in templateNode) {
+  const pageTemplateNodes = [...templateNode.content.querySelectorAll(`section.main`)];
+
+  const mainNode = document.querySelector(`.app .main`);
+  if (mainNode) {
+    let currentIndex = changePage(mainNode, pageTemplateNodes, FIRST_PAGE_INDEX);
+
+    document.addEventListener(`keydown`, (event) => {
+      const keyName = event.key;
+
+      if (keyName === `Alt`) {
+        return;
+      }
+
+      if (event.altKey) {
+        if (keyName === `ArrowLeft`) {
+          currentIndex = changePage(mainNode, pageTemplateNodes, currentIndex - 1, currentIndex);
+        }
+        if (keyName === `ArrowRight`) {
+          currentIndex = changePage(mainNode, pageTemplateNodes, currentIndex + 1, currentIndex);
+        }
+      }
+    });
+  }
+}
+
+/**
+ * Возвращает страницу по индексу экрана
+ *
+ * @param {HTMLElement[]} pageList Список страниц
+ * @param {number} index Индекс требуемой страницы
+ * @param {string} optionClass Дополнительный класс искомой страницы
+ * @return {HTMLElement}
+ */
+function getPage(pageList, index, optionClass) {
+  const pageOrder = [
+    `main--welcome`,
+    `main--level`,
+    `main--result`,
+  ];
+
+  let page;
+  if (index >= 0 || index < pageOrder.length) {
+    page = pageList.find((it) =>
+      it.classList.contains(pageOrder[index]) &&
+      (!optionClass || it.classList.contains(optionClass)));
+  }
+  return page;
+}
+
+/**
+ * Изменяет текущую страницу в DOM
+ *
+ * @param {HTMLElement} mainNode Родительский узел
+ * @param {HTMLElement[]} pageTemplateNodes Список шаблонов страниц
+ * @param {number} nextIndex Индекс следующей страницы
+ * @param {number} currentIndex Индекс текущей страницы
+ * @return {number}
+ */
+function changePage(mainNode, pageTemplateNodes, nextIndex, currentIndex) {
+  const nextPageTemplateNode = getPage(pageTemplateNodes, nextIndex);
+  if (nextPageTemplateNode) {
+    if (mainNode.children.length > 0) {
+      mainNode.removeChild(mainNode.lastChild);
+    }
+    mainNode.appendChild(document.importNode(nextPageTemplateNode, true));
+    return nextIndex;
+  }
+
+  return currentIndex;
+}
